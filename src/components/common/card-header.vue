ebfac5023255882b35c9d4ba19f593a41fe97b8a
iRuxu <rx6@qq.com>
2024-09-23 14:28:52 +0800

    chore:placeholder

--- src/components/common/card-header.vue
+++ src/components/common/card-header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="m-header">
         <a :href="homepage"><img class="u-logo" svg-inline src="@/assets/img/test.svg" /></a>
-        <el-divider class="u-title">Mii</el-divider>
+        <el-divider class="u-title">User Center</el-divider>
     </header>
 </template>
 
