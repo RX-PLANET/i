f0303894cfb6022b44b4797e19f966ad26b94719
iRuxu <rx6@qq.com>
2024-11-20 23:05:17 +0800

    chore:account

---
+++ src/components/account/common/footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer class="m-footer">© RX-PLANET</footer>
+</template>
+
+<script>
+export default {
+    name: "RX-Footer1",
+    props: [],
+    components: {},
+    data: function () {
+        return {};
+    },
+    computed: {},
+    watch: {},
+    methods: {},
+    created: function () {},
+    mounted: function () {},
+};
+</script>
+
+<style lang="less">
+.m-footer {
+    padding: 10px 0;
+    text-align: center;
+    font-size: 12px;
+    letter-spacing: 0.5px;
+    color: #999;
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Arial, sans-serif;
+}
+</style>
